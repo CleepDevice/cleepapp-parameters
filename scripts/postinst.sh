46976a73a1db67a29568d31f0eeed93b88959883
tang <tanguy.bonneau@gmail.com>
2023-03-10 12:37:30 +0100

    Update dependencies

--- scripts/postinst.sh
+++ scripts/postinst.sh
@@ -7,4 +7,4 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command failed with exit code $?."' ERR
 
-python3 -m pip install "workalendar>=16.0.0,<17.0.0" "pycountry-convert>=0.7.2,<0.8.0" "pytz>=2021.1,<2022.0" "reverse-geocode>=1.4.1,<1.5.0" "timezonefinder>=5.2.0,<5.3.0" "tzlocal>=2.1,<2.2"
+python3 -m pip install --trusted-host pypi.org "workalendar==17.0.0" "pycountry-convert>=0.7.2,<0.8.0" "pytz==2021.3" "reverse-geocode==1.4.1" "timezonefinder==5.2.0" "tzlocal==2.1"
