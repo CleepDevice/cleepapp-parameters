1127aba26585b83deaab498fa476ee8435937d88
tang <tanguy.bonneau@gmail.com>
2024-03-06 20:33:59 +0100

    Bump pytz

--- scripts/postinst.sh
+++ scripts/postinst.sh
@@ -7,4 +7,4 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command failed with exit code $?."' ERR
 
-python3 -m pip install --trusted-host pypi.org "workalendar==17.0.0" "pycountry-convert>=0.7.2,<0.8.0" "pytz==2021.3" "reverse-geocode==1.4.1" "timezonefinder==5.2.0" "tzlocal==2.1"
+python3 -m pip install --trusted-host pypi.org "workalendar==17.0.0" "pycountry-convert>=0.7.2,<0.8.0" "pytz==2024.1" "reverse-geocode==1.4.1" "timezonefinder==5.2.0" "tzlocal==2.1"
